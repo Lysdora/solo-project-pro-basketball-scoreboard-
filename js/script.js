b3b921f97d6e95e61ccb8d9e0c83ea4cad2ad93d
Lysdora <lysdora.web@gmail.com>
2023-07-05 09:59:25 +0200

    I create 3 functions for Home Score

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let scoreHomeEl = document.getElementById("score-home").textContent;
+let scoreHome = 0;
+let scoreGuest = 0;
+
+function addPlusOneHome() {
+  document.getElementById("score-home").textContent = scoreHome += 1;
+}
+
+function addPlusTwoHome() {
+  document.getElementById("score-home").textContent = scoreHome += 2;
+}
+
+function addPLusThreeHome() {
+  document.getElementById("score-home").textContent = scoreHome += 3;
+}
