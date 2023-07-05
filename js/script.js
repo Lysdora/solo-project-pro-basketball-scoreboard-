bfc34c483b872b59c0b377599fa9a061dff2289d
Lysdora <lysdora.web@gmail.com>
2023-07-05 15:39:16 +0200

    Add css to my project + button new game with function reset game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use-strict";
 let scoreHome = 0;
 let scoreGuest = 0;
+let reset = 0;
 
 //I create 3 functions to increment score to Home '+1 +2 and +3
 function addPlusOneHome() {
@@ -25,4 +26,19 @@ function addPlusTwoGuest() {
 
 function addPlusThreeGuest() {
   document.getElementById("score-guest").textContent = scoreGuest += 3;
+}
+
+//Add function reset
+function resetGame() {
+  //For score Home
+  document.getElementById("score-home").textContent = reset;
+  //For score Guest
+  document.getElementById("score-guest").textContent = reset;
+}
+
+//Hightligth winner function
+
+if (scoreHome !== nul) {
+  document.getElementById("score-home").style.color = "green";
+  console.log("Hello");
 }
